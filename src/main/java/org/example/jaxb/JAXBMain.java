c9fbe25896a4478e6d05f1e0613e7d4e29a8d4c0
Saranyar04 <srajendran.laba@solvd.com>
2024-01-10 09:38:02 -0500

    Added suggested changes

--- src/main/java/org/example/jaxb/JAXBMain.java
+++ src/main/java/org/example/jaxb/JAXBMain.java
@@ -12,14 +12,16 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import static org.example.jaxb.JAXBMarshaller.marshal;
+import static org.example.jaxb.JAXBMarshaller.unmarshal;
+
 public class JAXBMain {
 
     private static final Logger LOGGER = (Logger) LogManager.getLogger(JAXBMain.class);
     public static void main(String[] args) throws ParseException {
         PositionDAO positionDAO = new PositionDAO();
         Position position = positionDAO.getEntityByID(2);
-        JAXBMarshaller<Position> positionJAXBMarshaller = new JAXBMarshaller<>();
-        positionJAXBMarshaller.marshal(position, "src/main/resources/marshal-xml/position.xml");
+        marshal(position, "src/main/resources/marshal-xml/position.xml");
 
         NurseDAO nurseDAO = new NurseDAO();
         Nurse nurse1 = nurseDAO.getEntityByID(3);
@@ -30,29 +32,25 @@ public class JAXBMain {
         DepartmentDAO departmentDAO = new DepartmentDAO();
         Department department = departmentDAO.getEntityByID(2);
         department.setNursesList(nurseList);
-        JAXBMarshaller<Department> departmentJAXBMarshaller = new JAXBMarshaller<>();
-        departmentJAXBMarshaller.marshal(department, "src/main/resources/marshal-xml/department.xml");
+        marshal(department, "src/main/resources/marshal-xml/department.xml");
 
         SpecializationDAO specializationDAO = new SpecializationDAO();
         Specialization specialization = specializationDAO.getEntityByID(2);
-        JAXBMarshaller<Specialization> specializationJAXBMarshaller = new JAXBMarshaller<>();
-        specializationJAXBMarshaller.marshal(specialization, "src/main/resources/marshal-xml/specialization.xml");
+        marshal(specialization, "src/main/resources/marshal-xml/specialization.xml");
 
         PhysicianDAO physicianDAO = new PhysicianDAO();
         Physician physician = physicianDAO.getEntityByID(12);
-        JAXBMarshaller<Physician> physicianJAXBMarshaller = new JAXBMarshaller<>();
-        specializationJAXBMarshaller.marshal(physician, "src/main/resources/marshal-xml/physician.xml");
+        marshal(physician, "src/main/resources/marshal-xml/physician.xml");
 
         PatientDAO patientDAO = new PatientDAO();
         Patient patient = patientDAO.getEntityByID(12);
         Date date = new SimpleDateFormat("yyyy-MM-dd").parse("2000-10-10");
         java.sql.Date date1 = new java.sql.Date(date.getTime());
         patient.setDateOfBirth(date1);
-        JAXBMarshaller<Patient> patientJAXBMarshaller = new JAXBMarshaller<>();
-        patientJAXBMarshaller.marshal(patient, "src/main/resources/marshal-xml/patient.xml");
+        marshal(patient, "src/main/resources/marshal-xml/patient.xml");
 
         Patient patient1 = new Patient();
-        patient1 = patientJAXBMarshaller.unmarshal("src/main/resources/marshal-xml/patient.xml", Patient.class);
+        patient1 = unmarshal("src/main/resources/marshal-xml/patient.xml", Patient.class);
         LOGGER.info(patient1);
     }
 }
